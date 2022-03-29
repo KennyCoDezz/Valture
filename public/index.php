ae415be366f3532f20a8baaee7a8cd1428fe4083
Kenneth Lopez <88085103+KennyCoDezz@users.noreply.github.com>
2022-03-29 14:33:20 +0800

    added return date on book_overview.html

--- public/index.php
+++ public/index.php
@@ -1,17 +1,10 @@
 <head>
     <link rel="stylesheet" href="assets/fontawesome-free-6.0.0-beta2-web/css/all.min.css">
 </head>
-<style>
-    <?php include 'assets/css/homepage.css';
-    include 'assets/css/root.css';
-    include 'assets/css/navigation.css';
-    include 'assets/css/footer.css';
-    ?>
-</style>
 <?php
 include 'includes/unregistered-nav.html';
 //include 'pages/home/homepage.html';
-include 'pages/collections/book_overview.html';
+include 'pages/collections/book_reading_mode.html';
 include 'includes/main-footer.html';
 
 ?>
