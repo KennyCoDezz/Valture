ac828e982a96b4488ee44359faa52563bf647a62
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-03-31 18:57:15 +0800

    minor changes

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 </head>
 <?php
 include 'includes/registered-nav.html';
-include 'pages/home/homepage.html';
+//include 'pages/home/homepage.html';
 //include 'pages/collections/book_overview.html';
-include 'includes/main-footer.html';
+include 'pages/collections/book_reading_mode.html';
+include 'includes/footer.html';
 
 ?>
