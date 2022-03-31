43ffa8ef0eb9090c6a64a31c884aefe0ef032448
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-03-31 18:21:37 +0800

    changed some css designs in navigation

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
     <link rel="stylesheet" href="assets/fontawesome-free-6.0.0-beta2-web/css/all.min.css">
 </head>
 <?php
-include 'includes/unregistered-nav.html';
-//include 'pages/home/homepage.html';
-include 'pages/collections/book_overview.html';
+include 'includes/registered-nav.html';
+include 'pages/home/homepage.html';
+//include 'pages/collections/book_overview.html';
 include 'includes/main-footer.html';
 
 ?>
