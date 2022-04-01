87e8cefb9985d7a34f0a2c24859c23907e1ae605
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-01 14:59:29 +0800

    fixed media queries of navigation and separated icons

--- public/index.php
+++ public/index.php
@@ -1,11 +1,9 @@
-<head>
-    <link rel="stylesheet" href="assets/fontawesome-free-6.0.0-beta2-web/css/all.min.css">
-</head>
+
 <?php
 include 'includes/registered-nav.html';
-//include 'pages/home/homepage.html';
+include 'pages/home/homepage.html';
 //include 'pages/collections/book_overview.html';
-include 'pages/collections/book_reading_mode.html';
+//include 'pages/collections/book_reading_mode.html';
 include 'includes/footer.html';
 
 ?>
