1a5909d98dfbf673538f160a1027295e02177780
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-02 13:32:52 +0800

    removed some folders and moved files

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 
 <?php
 include 'includes/registered-nav.html';
-//include 'pages/home/homepage.html';
-//include 'pages/landing.html';
-include 'pages/collections/book_overview.html';
-//include 'pages/collections/book_reading_mode.html';
+//include 'pages/homepage.html';
+include 'pages/book_overview.html';
+//include 'pages/book_reading_mode.html';
 include 'includes/footer.html';
 
 ?>
