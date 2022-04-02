569dfb8feecac1879d964df2d5c0f745030eb5dd
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-02 15:16:38 +0800

    moved all files to front

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php
+include 'includes/registered-nav.html';
+//include 'pages/homepage.html';
+include 'pages/book_overview.html';
+//include 'pages/book_reading_mode.html';
+include 'includes/footer.html';
+
+?>
--- public/index.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-include 'includes/registered-nav.html';
-//include 'pages/homepage.html';
-include 'pages/book_overview.html';
-//include 'pages/book_reading_mode.html';
-include 'includes/footer.html';
-
-?>
