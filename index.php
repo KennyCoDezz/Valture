7d5150c9ac1ac8fbc0b0daa49116c102c63a11fc
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-03 08:53:51 +0800

    changed opacity in newsletter section

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 
 <?php
 include 'includes/registered-nav.html';
+//include 'includes/unregistered-nav.html';
 //include 'pages/homepage.html';
-include 'pages/book_overview.html';
-//include 'pages/book_reading_mode.html';
+//include 'pages/book_overview.html';
+include 'pages/book_reading_mode.html';
 include 'includes/footer.html';
 
 ?>
