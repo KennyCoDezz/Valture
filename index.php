259ed1506b4f8c0fac919f8764457174d17c240d
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-03 10:38:26 +0800

    added js on removing bookmark button on borrow

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 include 'includes/registered-nav.html';
 //include 'includes/unregistered-nav.html';
 //include 'pages/homepage.html';
-//include 'pages/book_overview.html';
-include 'pages/book_reading_mode.html';
+include 'pages/book_overview.html';
+//include 'pages/book_reading_mode.html';
 include 'includes/footer.html';
 
 ?>
