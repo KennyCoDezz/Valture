b4ecf21684ba69a260bf6cc3d2fe3d27d0d25c0b
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-04 23:00:36 +0800

    added admin-resources page (half finished)

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 
 <?php
+//include 'admin/admin-nav.html';
 include 'includes/registered-nav.html';
 //include 'includes/unregistered-nav.html';
+
 //include 'pages/homepage.html';
 //include 'pages/book_overview.html';
 include 'pages/book_reading_mode.html';
+//include 'admin/resources.html';
+
+//include 'admin/admin-footer.html';
 include 'includes/footer.html';
 
 ?>
