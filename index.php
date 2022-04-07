2f1708742a65ccb90dfa17c0c0fe0e1638416022
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-07 20:49:31 +0800

    finished logs and setting for borrowing/returning books

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ include 'admin/admin-nav.html';
 //include 'pages/homepage.html';
 //include 'pages/book_overview.html';
 //include 'pages/book_reading_mode.html';
-include 'admin/resources.html';
+//include 'admin/resources.html';
+//include 'admin/logs.html';
+include 'admin/settings.html';
 
 include 'admin/admin-footer.html';
 //include 'includes/footer.html';
