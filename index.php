47a8d6c8bc3a2b35f603c587e8809b8349692218
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-10 16:38:21 +0800

    finished admin, user accounts, and events table

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ include 'admin/admin-nav.html';
 //include 'admin/settings.html';
 //include 'admin/administrators.html';
 //include 'admin/admin-user_accounts.html';
-include 'admin/super_admin-user_accounts.html';
+//include 'admin/super_admin-user_accounts.html';
+include 'admin/events.html';
 
 include 'admin/admin-footer.html';
 //include 'includes/footer.html';
