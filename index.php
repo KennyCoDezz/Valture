d1d55d07313de18243402b2efbf7247f047219d6
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-12 14:17:12 +0800

    changed file structure and finished categories and subcategories

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 
 <?php
-//include 'admin/admin-nav.html';
-//include 'includes/registered-nav.html';
+include 'includes/registered-nav.html';
 //include 'includes/unregistered-nav.html';
 
-//include 'pages/homepage.html';
-//include 'pages/book_overview.html';
-//include 'pages/book_reading_mode.html';
-//include 'admin/resources.html';
-//include 'admin/logs.html';
-//include 'admin/settings.html';
-//include 'admin/administrators.html';
-//include 'admin/admin-user_accounts.html';
-//include 'admin/super_admin-user_accounts.html';
-//include 'admin/events.html';
-include 'admin/categories.html';
+//include 'homepage.html';
+include 'book_overview.html';
+//include 'book_reading_mode.html';
 
-//include 'admin/admin-footer.html';
-//include 'includes/footer.html';
+include 'includes/footer.html';
 
 ?>
