346bba5a812902e1b709a0f42b2f7a39143dbc77
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-15 14:40:04 +0800

    separated directories of assets of admin

--- admin/index.php
+++ admin/index.php
@@ -1,15 +1,16 @@
 <?php
 include 'admin-nav.html';
 
-//include 'admin/resources.html';
-//include 'admin/logs.html';
-//include 'admin/settings.html';
-//include 'admin/administrators.html';
-//include 'admin/admin-user_accounts.html';
-//include 'admin/super_admin-user_accounts.html';
-//include 'admin/events.html';
-include 'categories.html';
-//include 'admin/subcategories.html';
+//include 'resources.html';
+//include 'logs.html';
+//include 'settings.html';
+//include 'administrators.html';
+//include 'admin-user_accounts.html';
+include 'super_admin-user_accounts.html';
+//include 'events.html';
+//include 'categories.html';
+//include 'subcategories.html';
+//include 'reports.html';
 
 include 'admin-footer.html';
 
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'includes/registered-nav.html';
 //include 'includes/unregistered-nav.html';
 
 //include 'homepage.html';
-include 'book_overview.html';
+//include 'book_overview.html';
 //include 'book_reading_mode.html';
 
 include 'includes/footer.html';
