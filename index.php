7b30f0b3d7da18e971fad4ce27a2269e74efc05f
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-15 22:13:21 +0800

    added user profile page

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ include 'includes/registered-nav.html';
 //include 'includes/unregistered-nav.html';
 
 //include 'homepage.html';
-include 'book_overview.php';
+//include 'book_overview.php';
 //include 'book_reading_mode.html';
+include 'user_profile.html';
 
 include 'includes/footer.html';
 
