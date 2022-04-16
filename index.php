5b131a7dcf5012c3ddc7d17a69d96f02e8c25d63
Kenneth Lopez <kennethlopez200126@gmail.com>
2022-04-16 21:05:03 +0800

    added user profile, edit account, change password, and bookmark pages

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ include 'includes/registered-nav.html';
 //include 'homepage.html';
 //include 'book_overview.php';
 //include 'book_reading_mode.html';
-include 'user_profile.html';
+//include 'user_profile.html';
+//include 'edit_account.html';
+//include 'change_password.html';
+include 'bookmark.html';
 
 include 'includes/footer.html';
 
